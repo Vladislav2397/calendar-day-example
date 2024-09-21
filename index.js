ded3c9da47b0a9a06e11ca22afc36c55ca8072e2
Vladislav <53734286+Vladislav2397@users.noreply.github.com>
2024-09-21 19:59:14 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,45 @@
+function createEventItemFactory(title, color) {
+    return (timeStart, timeEnd) => {
+        const current = new Date().toISOString().slice(0, 10)
+
+        return {
+            start: current + ' ' + timeStart,
+            end: current + ' ' + timeEnd,
+            title,
+            color,
+        }
+    }
+}
+
+const sleepingEvent = createEventItemFactory('Sleeping', '#0000ff')
+const eatingItem = createEventItemFactory('Eating', '#ff0000')
+const designItem = createEventItemFactory('Design', '#00ff00')
+const freetimeItem = createEventItemFactory('Freetime', '#00ff00')
+const workingItem = createEventItemFactory('Working', '#000000')
+const subworkItem = createEventItemFactory('Subwork', '#ffffff')
+const nongadgettimeItem = createEventItemFactory('Nongadgettime', '#00ffff')
+
+
+function createEvents() {
+    const current = new Date().toISOString().slice(0, 10)
+
+    console.log('current', current);
+
+    return [
+        sleepingEvent('00:00', '07:00'),
+        eatingItem('07:00', '07:30'),
+        designItem('07:30', '09:00'),
+        freetimeItem('09:30', '10:00'),
+        workingItem('10:00', '13:00'),
+        eatingItem('13:00', '14:00'),
+        workingItem('14:00', '19:00'),
+        subworkItem('19:00', '20:00'),
+        freetimeItem('20:00', '22:00'),
+        nongadgettimeItem('22:00', '23:00'),
+        sleepingEvent('23:00', '23:59'),
+    ];
+}
+
 const ec = new EventCalendar(document.getElementById('ec'), {
     view: 'timeGridDay',
     headerToolbar: {
@@ -37,45 +79,3 @@ const ec = new EventCalendar(document.getElementById('ec'), {
     nowIndicator: true,
     selectable: true
 });
-
-
-function createEventItemFactory(title, color) {
-    return (timeStart, timeEnd) => {
-        const current = new Date().toISOString().slice(0, 10)
-
-        return {
-            start: current + ' ' + timeStart,
-            end: current + ' ' + timeEnd,
-            title,
-            color,
-        }
-    }
-}
-
-const sleepingEvent = createEventItemFactory('Sleeping', '#0000ff')
-const eatingItem = createEventItemFactory('Eating', '#ff0000')
-const designItem = createEventItemFactory('Design', '#00ff00')
-const freetimeItem = createEventItemFactory('Freetime', '#00ff00')
-const workingItem = createEventItemFactory('Working', '#000000')
-const subworkItem = createEventItemFactory('Subwork', '#ffffff')
-const nongadgettimeItem = createEventItemFactory('Nongadgettime', '#00ffff')
-
-function createEvents() {
-    const current = new Date().toISOString().slice(0, 10)
-
-    console.log('current', current);
-
-    return [
-        sleepingEvent('00:00', '07:00'),
-        eatingItem('07:00', '07:30'),
-        designItem('07:30', '09:00'),
-        freetimeItem('09:30', '10:00'),
-        workingItem('10:00', '13:00'),
-        eatingItem('13:00', '14:00'),
-        workingItem('14:00', '19:00'),
-        subworkItem('19:00', '20:00'),
-        freetimeItem('20:00', '22:00'),
-        nongadgettimeItem('22:00', '23:00'),
-        sleepingEvent('23:00', '23:59'),
-    ];
-}
