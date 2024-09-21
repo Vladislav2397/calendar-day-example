c644697001b57c99c4bdc64efd866ef5d7b41009
VladislavT <vt@burno.tech>
2024-09-21 09:26:09 +0300

    Updated codebase

--- index.js
+++ index.js
@@ -38,23 +38,43 @@ const ec = new EventCalendar(document.getElementById('ec'), {
     selectable: true
 });
 
+const sleepingEvent = createEventItemFactory('Sleeping', '#0000ff')
+const eatingItem = createEventItemFactory('Eating', '#ff0000')
+const designItem = createEventItemFactory('Design', '#00ff00')
+const freetimeItem = createEventItemFactory('Freetime', '#00ff00')
+const workingItem = createEventItemFactory('Working', '#000000')
+const subworkItem = createEventItemFactory('Subwork', '#ffffff')
+const nongadgettimeItem = createEventItemFactory('Nongadgettime', '#00ffff')
+
 function createEvents() {
     const current = new Date().toISOString().slice(0, 10)
 
     console.log('current', current);
 
     return [
-        {start: current + " 00:00", end: current + " 07:00", title: "Sleeping", color: '#0000ff'},
-        {start: current + " 07:00", end: current + " 07:30", title: "Eating", color: '#ff0000'},
-        {start: current + " 07:30", end: current + " 09:00", title: "Design", color: '#00ff00'},
-        {start: current + " 09:00", end: current + " 09:30", title: "Eating", color: '#ff0000'},
-        {start: current + " 09:30", end: current + " 10:00", title: "Freetime", color: '#00ff00'},
-        {start: current + " 10:00", end: current + " 13:00", title: "Working", color: '#000000'},
-        {start: current + " 13:00", end: current + " 14:00", title: "Eating", color: '#ff0000'},
-        {start: current + " 14:00", end: current + " 19:00", title: "Working", color: '#000000'},
-        {start: current + " 19:00", end: current + " 20:00", title: "Subwork", color: '#00ff00'},
-        {start: current + " 20:00", end: current + " 22:00", title: "Freetime", color: '#00ff00'},
-        {start: current + " 22:00", end: current + " 23:00", title: "Nongadgettime", color: '#00ffff'},
-        {start: current + " 23:00", end: current + " 23:59", title: "Sleeping", color: '#0000ff'},
+        sleepingEvent('00:00', '07:00'),
+        eatingItem('07:00', '07:30'),
+        designItem('07:30', '09:00'),
+        freetimeItem('09:30', '10:00'),
+        workingItem('10:00', '13:00'),
+        eatingItem('13:00', '14:00'),
+        workingItem('14:00', '19:00'),
+        subworkItem('19:00', '20:00'),
+        freetimeItem('20:00', '22:00'),
+        nongadgettimeItem('22:00', '23:00'),
+        sleepingEvent('23:00', '23:59'),
     ];
+}
+
+function createEventItemFactory(title, color) {
+    return (timeStart, timeEnd) => {
+        const current = new Date().toISOString().slice(0, 10)
+
+        return {
+            start: current + ' ' + timeStart,
+            end: current + ' ' + timeEnd,
+            title,
+            color,
+        }
+    }
 }
